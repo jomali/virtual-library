5b4a4797cf7d04b79984db79d70b5aab904ba4b2
J. Francisco Martín <jfm.lisaso@gmail.com>
2021-01-23 09:51:50 +0100

    chore: Add client configurations

---
+++ client/.eslintrc.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+
+const getAllDirs = (dir) => {
+  const directoryPath = path.join(__dirname, dir);
+  const files = fs.readdirSync(directoryPath);
+  return files.map((value) => value.split('.')[0]);
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'react-app'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            // prints 'dir,dir/**' for each dir
+            pattern: `{${getAllDirs('src').join(',')},${getAllDirs('src')
+              .map((dir) => `${dir}/**`)
+              .join(',')}}`,
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'react/jsx-fragments': 'error',
+    'react/jsx-no-useless-fragment': 'error',
+  },
+}
